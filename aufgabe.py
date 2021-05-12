19f61d903d93e3324fab6bdc1f807727d09ba99c
te077 <te077@uni-rostock.de>
2021-05-12 15:48:52 +0000

    Update aufgabe.py

---
+++ aufgabe.py
@@ -0,0 +1,27 @@
+# aufgabe 1
+# hallo firat
+
+class RootCoordinator(object):
+
+    def __init__(self):
+        self.message = message
+        self.children = []
+
+
+
+class Coordinator(object):
+
+    def __init__(self):
+        self.message = message
+        self.parent = parent
+        self.children = []
+
+
+class Simulator(object):
+
+    def __init__(self):
+        self.message = message
+        self.parent = parent
+        self.tonie = tonie
+        self.states = []
+
