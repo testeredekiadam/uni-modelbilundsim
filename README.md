d61bda9d4476f45035cda78bd328b6fff9c15007
testeredekiadam <erkolfirat@gmail.com>
2021-05-18 23:37:58 +0200

    MOSI Hausaufgabe 2

--- README.md
+++
@@ -1,5 +0,0 @@
-There is nothing interesting to read here
-
-please continue
-
-model.java to override simulator.java
--- ha2/src/modell/ModelTry.java
+++ ha2/src/modell/ModelTry.java
@@ -9,6 +9,7 @@ public class ModelTry {
 		RootCoordinator rCoord = new RootCoordinator("Root-Coordinator", 1000);
 		Coordinator rModell = new Coordinator(rCoord, "Root-Modell");
 		
+		
 		Region[][] field = new Region[21][21];
 		for(int i = 0; i < 21; i++) {
 			for(int j = 0; j < 21; j++) {
@@ -17,7 +18,6 @@ public class ModelTry {
 					field[i][j] = new Region(rModell, tName);
 			
 				}catch(Exception e) {
-					System.out.println("Hallo");
 				}
 			}
 		}
