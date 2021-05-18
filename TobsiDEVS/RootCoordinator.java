6331bcf9d5cc4bd888c9ceb3f789b99c774eed84
te077 <te077@uni-rostock.de>
2021-05-18 14:18:45 +0000

    Upload New File

---
+++ TobsiDEVS/RootCoordinator.java
@@ -0,0 +1,15 @@
+package simulator;
+
+public abstract class RootCoordinator extends Coordinator {
+
+	public RootCoordinator(Node parent, String name) {
+		super(null, name);
+	}
+	
+	public void initializeNextStep() {
+		for(Node node : children) {
+			node.receiveMessage(nachricht);
+		}
+	}
+
+}
