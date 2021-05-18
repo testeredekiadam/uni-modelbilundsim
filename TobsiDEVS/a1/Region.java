a7553ce87be7e4d5e1104e050e46279438c8fb5e
te077 <te077@uni-rostock.de>
2021-05-18 17:56:09 +0000

    Upload New File

---
+++ TobsiDEVS/a1/Region.java
@@ -0,0 +1,99 @@
+package simulator;
+
+import java.util.Random;
+
+public class Region extends Simulator {
+
+	public Region(Node parent, String name) {
+		super(parent, name);
+	}
+	
+	
+	@Override
+	public String lambda(String state) {
+		
+		return "return";
+		// TODO
+	}
+	
+	@Override
+	public void delta_int(String state) {
+		
+		// wood returns infinity
+		
+		if (state == "Meadow") {
+			state = "Wood";
+		}
+		
+		else if (state == "Fire") {
+			state = "Meadow";
+		}
+		
+		else {
+			state = "Wood";
+		}
+		
+	}
+	
+	@Override
+	// changes state
+	public void delta_ext(String state, long elapsedTime, String x) {
+		
+		if (x == "MatchMessage") {
+			if (state == "Fire") {
+				;
+			}
+			else {
+				// 1% warscheinlichkeit , dass sich Region enzündet
+				Random match = new Random();
+				int randMatchZahl = 1 + match.nextInt(100);
+				if (randMatchZahl <= 1) {
+					state = "Fire";
+				}
+			}
+		}
+		
+		else if (x == "NeighborFireMessage") {
+			if (state == "Fire") {
+				;
+			}
+			// EineRegionim  ZustandWoodkann  aberauch anfangen zu brennen mit einer 10% 
+			// Wahrscheinlichkeit, wenn eine Nachbarregion eine Feu-ernachricht sende
+			else if (state == "Wood") {
+				Random neighbor = new Random();
+				int randNeighborZahl = 1 + neighbor.nextInt(100);
+				if (randNeighborZahl <= 20) {
+					state = "Fire";
+				}
+				
+			}
+			
+			// Befindet sich eine Region im ZustandMeadow, fängt sie beim Eingang 
+			// einerFeuernachricht einer Nachbarregion immer an zu brennen.
+			else if (state == "Meadow") {
+				state = "Fire";
+			}
+		}
+		
+		
+	}
+	
+	@Override
+	public long timeAdvance(String state) {
+		
+		// Wood returns infinity
+		
+		if (state == "Meadow") {
+			return 5;
+		}
+		
+		else if (state == "Fire") {
+			return 11;
+		}
+		
+		else {
+			return 0;
+		}
+	}
+
+}
