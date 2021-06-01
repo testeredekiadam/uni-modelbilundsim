528fb8b68098a7b17f013e45e8af40cd011ff2cd
testeredekiadam <erkolfirat@gmail.com>
2021-06-01 18:03:59 +0200

    le grande finale

--- ha3/src/main/java/FirstReactionMethod.java
+++ ha3/src/main/java/FirstReactionMethod.java
@@ -4,14 +4,15 @@ import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 import java.util.Random;
 
-
-public class DirectMethod {
-
+public class FirstReactionMethod {
     long wald;
     long feuer;
+    double time_r0;
+    double time_r1;
+    double time_r2;
     double next_time;
     double time;
-    long middle_sum;//für experiment
+    long middle_sum;
 
     public long getMiddle_sum(){
         return this.middle_sum;
@@ -21,16 +22,15 @@ public class DirectMethod {
         this.middle_sum = temp;
     }
 
-    public DirectMethod(long wald, long feuer) {
+
+    public FirstReactionMethod(long wald, long feuer){
         this.wald = wald;
         this.feuer = feuer;
     }
 
     //Wald -> 2Wald @ 20
-    public long r_0() {
-
-        return (this.wald*20);
-
+    public double r_0(){
+        return (this.wald * 20);
     }
 
     //Wald + Feuer ->  2Feuer @ 0.01
@@ -41,73 +41,50 @@ public class DirectMethod {
 
     //Feuer ->  @ 20
     public double r_2() {
+
         return (this.feuer*20);
     }
 
-    //Summe
-    public double a_sum() {
-        return (r_0() + r_1() + r_2());
-    }
 
-
-    //Reaktion
-    public void reaktion() {
+    public double expVer(double rate){
         double x;
-
-
-        double rate = a_sum(); //Lambda
 
         //U(0,1) Gleichverteilung
         Random random = new Random();
         double y = random.nextDouble();
 
-        /*      Begründung:
-        *
-        * Zuerst berecehnet man die Exponentialverteilung (0,1)
-        * Die cdf-Formel mit der unteren Grenze l und der oberen Grenze u mit l >= 0 und u > l
-        * und F(x) = y ist U(0,1)
-        *
-        *               (e^(-rate*l) - e^(-rate*x))
-        * F(x) = y = ------------------------------------
-        *               (e^(-rate*l) - e^(-rate*u))
-        *
-        * nach der Umwandlung
-        *
-        *       ln[ e^(-rate*l) - (e^(-rate*l) - e^(-rate*u))*y ]
-        * x = -----------------------------------------------------
-        *                       -rate
-        *
-        * mitder unteren Grenze 0 und der oberen Grenze 1
-        *
-        *        ln[ 1 - (1 - e^(-rate))*y ]
-        * x = ---------------------------------
-        *              -rate
-        *
-        */
         x = (Math.log(1-(1-Math.exp(-rate))*y))/(-rate); //Works
         //Zeitsinkrementierer
-        this.next_time = x;
-
-        //Gleichverteilung (0,a_sum)
-        double xj;
-        xj = random.nextDouble() * a_sum();
-
-        //Minimale j bestimmen
-        if(r_0() > xj){
-            this.wald+=1;
-
-        }
-        else if((r_0()+r_1()) > xj){
-            this.wald-=1;
-            this.feuer+=1;
-        }
-        else if((r_0()+r_1()+r_2()) > xj){
-            this.feuer-=1;
-        }
+        return x;
     }
 
-    //Direct Method von SSA
-    public void directMethod(double lim){
+    public void reaktion(){
+        Double[] array;
+        this.time_r0 =  expVer(r_0());
+        this.time_r1 = expVer(r_1());
+        this.time_r2 = expVer(r_2());
+        array = new Double[]{this.time_r0, this.time_r1, this.time_r2};
+        double min = array[0];
+        for(int i = 1; i < 3 ; i++){
+            if(array[i]<min){
+                min = array[i];
+            }
+        }
+
+        if(min == array[0]){
+            this.wald+=1;
+        }else if(min == array[1]){
+            this.wald-=1;
+            this.feuer+=1;
+        }else if(min == array[2]){
+            this.feuer-=1;
+        }
+
+
+        this.next_time = min;
+    }
+
+    public void firstReactionMethod(double lim){
         long avgsum = 0;
         double writingCount = 0.01;
 
@@ -117,9 +94,11 @@ public class DirectMethod {
         DecimalFormat df = new DecimalFormat("#.##", otherSymbols);
 
         //Adresse der CSV-datei
-        String filePath = "directMethod.csv";
+        String filePath = "firstReactionMethod.csv";
+
         //Die Datei entleeren
         FileWriter fileWriter = null;
+
         try{
             fileWriter = new FileWriter(filePath);
             //HEADER
@@ -176,9 +155,11 @@ public class DirectMethod {
             e.printStackTrace();
         }
         setMiddle_sum(avgsum);
+
     }
 
 
 
-}
 
+
+}
--- ha3/src/main/java/Main.java
+++ ha3/src/main/java/Main.java
@@ -2,36 +2,69 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
+
         double final_time = 1.0; // Zeiteinheiten bis zum Zeitpunkt 1.0
-        long sum;
         int wiederholung = 100;
         int initialStep = 0;
-        double stdab;
-        double durchschnitt;
-        StandardAbweichung standardAbweichung = new StandardAbweichung(wiederholung);
 
-        //Experiment
+        long sumA2;
+        double stdabA2;
+        double durchschnittA2;
+        StandardAbweichung standardAbweichungA2 = new StandardAbweichung(wiederholung);
+
+        double stdabA3;
+        double durchschnittA3;
+        long sumA3;
+        StandardAbweichung standardAbweichungA3 = new StandardAbweichung(wiederholung);
+
+
+        //Experiment Aufgabe-2
         while(initialStep<wiederholung) {
             DirectMethod dm = new DirectMethod(1000, 1000);
             dm.directMethod(final_time);
-            standardAbweichung.insertElement(initialStep, dm.getMiddle_sum());
+            standardAbweichungA2.insertElement(initialStep, dm.getMiddle_sum());
             initialStep++;
         }
 
-        sum = standardAbweichung.sum();
-        durchschnitt = standardAbweichung.durchschnitt();
-        stdab = standardAbweichung.stdAbw();
+        //Experimet Aufgabe-3
 
-        System.out.println("Sum =  " + sum);
-        System.out.println("Durchschnitt =  " + durchschnitt);
-        System.out.println("Standardabweichung = " + stdab);
+        initialStep = 0;
+        while(initialStep<wiederholung) {
+            FirstReactionMethod fr = new FirstReactionMethod(1000, 1000);
+            fr.firstReactionMethod(final_time);
+            standardAbweichungA3.insertElement(initialStep, fr.getMiddle_sum());
+            initialStep++;
+        }
+
+
+        //Ergebnisse A2
+        sumA2 = standardAbweichungA2.sum();
+        durchschnittA2 = standardAbweichungA2.durchschnitt();
+        stdabA2 = standardAbweichungA2.stdAbw();
+
+        System.out.println("Direct Method");
+        System.out.println("Sum =  " + sumA2);
+        System.out.println("Durchschnitt =  " + durchschnittA2);
+        System.out.println("Standardabweichung = " + stdabA2);
+
+        //Ergebnisse A3
+        sumA3 = standardAbweichungA3.sum();
+        durchschnittA3 = standardAbweichungA3.durchschnitt();
+        stdabA3 = standardAbweichungA3.stdAbw();
+
+        System.out.println("\n\nFirst Reaction Method");
+        System.out.println("Sum =  " + sumA3);
+        System.out.println("Durchschnitt =  " + durchschnittA3);
+        System.out.println("Standardabweichung = " + stdabA3);
 
         /*Graph
         *
         */
 
+
+        /*
        SwingUtilities.invokeLater(()->{
-            Plotter example = new Plotter("SSA");
+            Plotter directMethod = new Plotter("SSA-directMethod");
             example.setAlwaysOnTop(true);
             example.pack();
             example.setSize(2000, 1000);
@@ -39,6 +72,6 @@ public class Main {
             example.setVisible(true);
         });
 
-
+*/
     }
 }
