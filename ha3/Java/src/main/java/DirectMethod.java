62d55f49c3ba0e42702ca71d5dbe67c874cafb01
fe189 <firat.erkol@uni-rostock.de>
2021-06-06 17:30:45 -0500

    ignore

---
+++ ha3/Java/src/main/java/DirectMethod.java
@@ -0,0 +1,184 @@
+import java.io.FileWriter;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+import java.util.Random;
+
+
+public class DirectMethod {
+
+    long wald;
+    long feuer;
+    double next_time;
+    double time;
+    long middle_sum;//für experiment
+
+    public long getMiddle_sum(){
+        return this.middle_sum;
+    }
+
+    public void setMiddle_sum(long temp){
+        this.middle_sum = temp;
+    }
+
+    public DirectMethod(long wald, long feuer) {
+        this.wald = wald;
+        this.feuer = feuer;
+    }
+
+    //Wald -> 2Wald @ 20
+    public long r_0() {
+
+        return (this.wald*20);
+
+    }
+
+    //Wald + Feuer ->  2Feuer @ 0.01
+    public double r_1() {
+
+        return (this.wald*this.feuer*0.01);
+    }
+
+    //Feuer ->  @ 20
+    public double r_2() {
+        return (this.feuer*20);
+    }
+
+    //Summe
+    public double a_sum() {
+        return (r_0() + r_1() + r_2());
+    }
+
+
+    //Reaktion
+    public void reaktion() {
+        double x;
+
+
+        double rate = a_sum(); //Lambda
+
+        //U(0,1) Gleichverteilung
+        Random random = new Random();
+        double y = random.nextDouble();
+
+        /*      Begründung:
+        *
+        * Zuerst berecehnet man die Exponentialverteilung (0,1)
+        * Die cdf-Formel mit der unteren Grenze l und der oberen Grenze u mit l >= 0 und u > l
+        * und F(x) = y ist U(0,1)
+        *
+        *               (e^(-rate*l) - e^(-rate*x))
+        * F(x) = y = ------------------------------------
+        *               (e^(-rate*l) - e^(-rate*u))
+        *
+        * nach der Umwandlung
+        *
+        *       ln[ e^(-rate*l) - (e^(-rate*l) - e^(-rate*u))*y ]
+        * x = -----------------------------------------------------
+        *                       -rate
+        *
+        * mitder unteren Grenze 0 und der oberen Grenze 1
+        *
+        *        ln[ 1 - (1 - e^(-rate))*y ]
+        * x = ---------------------------------
+        *              -rate
+        *
+        */
+        x = (Math.log(1-(1-Math.exp(-rate))*y))/(-rate); //Works
+        //Zeitsinkrementierer
+        this.next_time = x;
+
+        //Gleichverteilung (0,a_sum)
+        double xj;
+        xj = random.nextDouble() * a_sum();
+
+        //Minimale j bestimmen
+        if(r_0() > xj){
+            this.wald+=1;
+
+        }
+        else if((r_0()+r_1()) > xj){
+            this.wald-=1;
+            this.feuer+=1;
+        }
+        else if((r_0()+r_1()+r_2()) > xj){
+            this.feuer-=1;
+        }
+    }
+
+    //Direct Method von SSA
+    public void directMethod(double lim){
+        long avgsum = 0;
+        double writingCount = 0.01;
+
+        Locale currentLocale = Locale.getDefault();
+        DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(currentLocale);
+        otherSymbols.setDecimalSeparator('.');
+        DecimalFormat df = new DecimalFormat("#.##", otherSymbols);
+
+        //Adresse der CSV-datei
+        String filePath = "directMethod.csv";
+        //Die Datei entleeren
+        FileWriter fileWriter = null;
+        try{
+            fileWriter = new FileWriter(filePath);
+            //HEADER
+            fileWriter.append("Time, Wald, Feuer");
+            fileWriter.append("\n");
+            //Anfangszustand
+            fileWriter.append(this.time + "," + this.wald+ "," + this.feuer);
+            fileWriter.append("\n");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                fileWriter.flush();
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        while(this.time < lim){
+            avgsum++;
+            reaktion();
+            this.time += this.next_time;
+
+            if(this.time>writingCount){
+
+
+                try{
+
+                    fileWriter.append(df.format(this.time) + "," + this.wald+ "," + this.feuer);
+                    fileWriter.append("\n");
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }finally{
+                    try{
+                        fileWriter.flush();
+
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+
+
+                writingCount += 0.01;
+            }
+
+
+        }
+        try{
+            fileWriter.close();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        setMiddle_sum(avgsum);
+    }
+
+
+
+}
+
---
+++ ha3/Java/src/main/java/Plotter.java
@@ -0,0 +1,74 @@
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Plotter extends JFrame{
+    String title;
+    public Plotter(String appTitle){
+        this.title = appTitle;
+
+        DefaultCategoryDataset dataset = getData();
+
+        JFreeChart lineChart = ChartFactory.createLineChart("Waldbrandmodel-SSA", "Time", "Amount", dataset);
+
+        try {
+            ChartUtilities.saveChartAsJPEG(new File("graph.jpg"), lineChart, 2000, 1000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ChartPanel panel = new ChartPanel(lineChart);
+        setContentPane(panel);
+    }
+
+    public DefaultCategoryDataset getData(){
+        String stream1 = "Wald";
+        String stream2 = "Feuer";
+
+        int wald;
+        int feuer;
+
+
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        String filePath = "directMethod.csv";
+        BufferedReader reader;
+        FileReader fileReader = null;
+        String line;
+
+        try{
+
+            fileReader = new FileReader(filePath);
+            reader = new BufferedReader(fileReader);
+            reader.readLine();
+            while((line = reader.readLine()) != null){
+                String[] row = line.split(",");
+                wald = Integer.parseInt(row[1]);
+                feuer = Integer.parseInt(row[2]);
+                dataset.addValue(wald, stream1, row[0]);
+                dataset.addValue(feuer, stream2, row[0]);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            try{
+                fileReader.close();
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return dataset;
+
+    }
+
+
+}
